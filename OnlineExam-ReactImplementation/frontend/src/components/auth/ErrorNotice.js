f4bfebc8d43afd69f40220d373282c5a2d98c84c
aartiwani <waniaarti.cdac@gmail.com>
2021-02-09 20:25:37 +0530

    Final Project Submission 9/2/2021 commit

--- OnlineExam-ReactImplementation/frontend/src/components/auth/ErrorNotice.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ErrorNotice(props) {
-    return (
-        <div className="error-notice">
-            <span>{props.message}</span>
-        </div>
-    );
-}
--- OnlineExam-ReactImplementation/frontend/src/components/layout/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-    return (
-        <div id="footer">
-           <span>Made in India.</span> 
-        </div>
-    )
-}
